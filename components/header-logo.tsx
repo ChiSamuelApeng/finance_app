347a94cd4f5289544a78acc0b350f16700ccd09b
Chisamuel651 <chijoe0306@gmail.com>
2024-05-20 14:17:49 +0100

    successfully implemented login and signup pages

---
+++ components/header-logo.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export const HeaderLogo = () => {
+    return (
+        <Link href='/'>
+            <div>
+                <div className="items-center hidden lg:flex">
+                    <Image src='/logo.svg' alt="Logo" width={38} height={38} />
+                    <p className="font-semibold text-white text-2xl ml-2.5">Finanzer</p>
+                </div>
+            </div>
+        </Link>
+    )
+}
